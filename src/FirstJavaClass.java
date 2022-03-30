5c651d82ee690e189b4044a579f5359ba99ca6ad
Graham Ertabak <grahamertabak@gmail.com>
2022-03-30 18:13:25 -0400

    Third commit is add some new lines

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,6 +1,8 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github!");
+        System.out.println("Adding the new code line");
+        System.out.println("This is working !");
     }
 
 }
